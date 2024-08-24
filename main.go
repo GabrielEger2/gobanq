ecb24d575dca9e492a16bf3b59cd9dbbc4e6f9ef
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-08-23 22:21:53 -0300

    feat: add viper env files and validation

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/GabrielEger2/gobanq/api"
 	db "github.com/GabrielEger2/gobanq/db/sqlc"
+	"github.com/GabrielEger2/gobanq/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@db:5432/gobanq?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	testDB, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(testDB)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
