710e9868ec76e87cb3812c1e41fad974400a5c54
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2024-08-31 15:18:11 -0300

    feat: add transfer route

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/GabrielEger2/gobanq/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -13,9 +15,15 @@ type Server struct {
 func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
 }
